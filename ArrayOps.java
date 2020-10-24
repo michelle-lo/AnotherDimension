65c4c01df35f7371737ba14af2ab68ebff55ce93
michelle-lo <mlo20@stuy.edu>
2020-10-24 14:20:17 -0400

    sum and largest now work

--- ArrayOps.java
+++ ArrayOps.java
@@ -3,6 +3,7 @@ public class ArrayOps {
 
   }
 
+//4c: returns the sum of individual values in array
   public static int sum(int[] arr) {
     int sum = 0;
     for (int i = 0; i < arr.length; i++) {
@@ -10,4 +11,17 @@ public class ArrayOps {
     }
     return sum;
   }
+
+//4b: returns the largest value in array
+  public static  int largest(int[]arr) {
+    int max = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return max;
+  }
+
+
 }
